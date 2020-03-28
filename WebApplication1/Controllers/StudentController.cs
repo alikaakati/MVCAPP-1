50fde1a26c1db66d9a72cbe069db341137d03afd
Ali Kaakati <kaakatiali@gmail.com>
2020-03-28 18:44:12 +0200

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MyStudentName() {
+            Student student = new Student();
+            student.age = 21;
+            student.studentID = 1;
+            student.studentName = "Ali kaakati";
+            return View(student);
+        }
+    }
+}
